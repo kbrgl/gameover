6c68ebfada043fba1d0eb9157a36d278d2eb6c37
Kabir Goel <kabirgoel.kg@gmail.com>
2022-07-23 16:17:18 -0700

    feat: init command

---
+++ cmd/init.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/spf13/cobra"
+)
+
+var (
+	initCmd = &cobra.Command{
+		Use:   "init",
+		Short: "Initialize ~/.gameover.",
+		Run: func(cmd *cobra.Command, args []string) {
+			tmp, err := ioutil.TempDir("", "gameover")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			os.RemoveAll(gameoverDir(""))
+			_, err = git.PlainClone(tmp, false, &git.CloneOptions{
+				URL: "https://github.com/kbrgl/gameover.git",
+			})
+			if err != nil {
+				fmt.Printf("error: %s\n", err)
+				os.Exit(1)
+			}
+			err = os.Rename(filepath.Join(tmp, ".gameover"), gameoverDir(""))
+			if err != nil {
+				fmt.Printf("failed to rename: %s\n", err)
+				os.Exit(1)
+			}
+			err = os.RemoveAll(tmp)
+			if err != nil {
+				fmt.Printf("failed to delete directory: %s\n", err)
+				os.Exit(1)
+			}
+			fmt.Println("Initialized gameover in", gameoverDir(""))
+			fmt.Println("Add 'source ~/.gameover/env' to your ~/.profile or whatever shell init script you use")
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
