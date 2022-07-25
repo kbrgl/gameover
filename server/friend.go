a331e56c1608cdd2772248a19eb3d0f0f7cfa2f0
Kabir Goel <kabirgoel.kg@gmail.com>
2022-07-25 01:44:00 -0700

    feat(server): flesh out friend route

--- server/friend.go
+++ server/friend.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,15 +12,24 @@ func friend(c echo.Context) error {
 	if friendUsername == "" {
 		return echo.NewHTTPError(http.StatusBadRequest, "missing username")
 	}
+
 	var friend User
 	err := db.Where("username = ?", friendUsername).First(&friend).Error
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid username")
 	}
+
 	var user User
+	id := c.Get("user").(*jwt.Token).Claims.(*jwt.StandardClaims).Subject
+	err = db.Where("id = ?", id).First(&user).Error
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get user")
+	}
+
 	err = db.Model(&user).Association("Friends").Append(&friend)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "failed to add friend")
 	}
-	return c.NoContent(201)
+
+	return c.NoContent(http.StatusOK)
 }
